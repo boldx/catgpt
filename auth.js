7b83a8b21d464a2e7e40c34db7be3b09f766a96e
boldx <boldx@rucsok.hu>
2024-10-13 17:54:21 +0200

    oops

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const url = require('url');
+const axios = require('axios');
 
 
 const oauth_api = axios.create({
@@ -24,18 +25,19 @@ app.get('/', (req, res) => {
 
 
 app.get('/auth', async (req, res) => {
-    if(!req.query.code) {
-        res.redirect(url.format({pathname:"/authorize", query: {response_type: 'code', client_id: process.env.clientID, redirect_uri: process.env.redirectURL}}));
-    } else {
+    //if(!req.query.code) {
+    //    res.redirect(url.format({pathname:"/authorize", query: {response_type: 'code', client_id: process.env.clientID, redirect_uri: process.env.redirectURL}}));
+    //} else {
         const params = new url.URLSearchParams({grant_type: 'authorization_code', code: req.query.code, redirect_uri: process.env.redirectURL});
         try {
             let resp = await oauth_api.post('/token', params.toString());
             fs.writeFileSync(process.env.refreshTokenFile, resp.data.refresh_token);
+            console.log('Refresh token acquired!')
         } catch (error) { 
             console.error(`Acquiring initial tokens failed: ${error} ${JSON.stringify(error.response?.data)}`);
         };
         res.redirect('/');
-    }
+    //}
 })
 
 
